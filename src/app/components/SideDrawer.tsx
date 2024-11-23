a4c3c3b09845ba6941a2db0d2f293dd2c1220e9d
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2024-11-23 17:35:05 +0200

    fix: Simplify component, debounce not needed for now

--- src/app/components/SideDrawer.tsx
+++ src/app/components/SideDrawer.tsx
@@ -1,41 +1,26 @@
 'use client';
 import { SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
 import { Input } from '@/components/ui/input';
-
 import { EmergencyEvent } from '../types/emergency';
-import {
-	Dispatch,
-	RefObject,
-	SetStateAction,
-	useEffect,
-	useState,
-} from 'react';
+import { Dispatch, RefObject, SetStateAction, useState } from 'react';
 import { findMarkerByPosition, searchFrom } from '@/lib/utils';
 import { Map, Marker } from 'leaflet';
-
 interface SideDrawerProps {
 	content: EmergencyEvent[];
 	map: RefObject<Map>;
 	setSheetOpen: Dispatch<SetStateAction<boolean>>;
 	markers: RefObject<Marker[]>;
 }
-
 const SideDrawer: React.FC<SideDrawerProps> = ({
 	content,
 	map,
 	markers,
 	setSheetOpen,
 }) => {
-	const [results, setResults] = useState<EmergencyEvent[]>(content || []);
-	const [filteredResults, setFilteredResults] = useState<EmergencyEvent[]>(
-		[],
-	);
-	const [debouncedSearch, setDebouncedSearch] = useState('');
-
+	const [searchTerm, setSearchTerm] = useState('');
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setDebouncedSearch(e.target.value);
+		setSearchTerm(e.target.value);
 	};
-
 	const handleClick = (event: EmergencyEvent) => {
 		map.current?.flyTo(event.position, 15);
 		setSheetOpen(false);
@@ -44,45 +29,18 @@ const SideDrawer: React.FC<SideDrawerProps> = ({
 			foundMarker ? foundMarker.openPopup() : null;
 		});
 	};
-
-	useEffect(() => {
-		if (content?.length) {
-			setResults(content);
-			setFilteredResults(content);
-		}
-	}, [content]);
-
-	useEffect(() => {
-		const handler = setTimeout(() => {
-			let updatedResults = results;
-
-			if (debouncedSearch.trim().length > 0) {
-				updatedResults = searchFrom(results, debouncedSearch);
-			}
-
-			updatedResults.sort(
-				(a, b) =>
-					new Date(b.timestamp).getTime() -
-					new Date(a.timestamp).getTime(),
-			);
-
-			setFilteredResults(updatedResults);
-		}, 300); // 300ms debounce delay for better ux
-
-		return () => clearTimeout(handler);
-	}, [debouncedSearch, results]);
 	return (
 		<SheetContent className="z-[1000]">
 			<SheetHeader className="mb-3">
 				<SheetTitle>Listaus tapahtumista</SheetTitle>
 				<Input
 					placeholder="Tyyppi, paikka, aika"
-					value={debouncedSearch}
+					value={searchTerm}
 					onChange={handleChange}
 				/>
 			</SheetHeader>
 			<div className="overflow-y-auto max-h-full">
-				{filteredResults.map((item) => {
+				{searchFrom(content, searchTerm).map((item) => {
 					return (
 						<div
 							key={item.id}
@@ -99,5 +57,4 @@ const SideDrawer: React.FC<SideDrawerProps> = ({
 		</SheetContent>
 	);
 };
-
 export default SideDrawer;
