ae71c0c7ddb9e828bf908f26cfe31b8e2cfe59ae
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2024-11-23 22:11:14 +0200

    monorepo

--- apps/web/src/app/components/SideDrawer.tsx
+++ apps/web/src/app/components/SideDrawer.tsx
@@ -1,27 +1,25 @@
-'use client'
-import {
-	SheetContent,
-	SheetHeader,
-	SheetTitle,
-} from "@/components/ui/sheet"
-import { Input } from "@/components/ui/input"
+'use client';
+import { SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import { Input } from '@/components/ui/input';
 
-import { EmergencyEvent } from "../types/emergency";
-import { useEffect, useState } from "react";
-import { searchFrom } from "@/lib/utils";
+import { EmergencyEvent } from '../types/emergency';
+import { useEffect, useState } from 'react';
+import { searchFrom } from '@/lib/utils';
 
 interface SideDrawerProps {
-	content: EmergencyEvent[]
+	content: EmergencyEvent[];
 }
 
 const SideDrawer: React.FC<SideDrawerProps> = ({ content }) => {
 	const [results, setResults] = useState<EmergencyEvent[]>(content || []);
-	const [filteredResults, setFilteredResults] = useState<EmergencyEvent[]>([]);
+	const [filteredResults, setFilteredResults] = useState<EmergencyEvent[]>(
+		[],
+	);
 	const [debouncedSearch, setDebouncedSearch] = useState('');
 
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setDebouncedSearch(e.target.value)
-	}
+		setDebouncedSearch(e.target.value);
+	};
 
 	useEffect(() => {
 		if (content?.length) {
@@ -38,7 +36,11 @@ const SideDrawer: React.FC<SideDrawerProps> = ({ content }) => {
 				updatedResults = searchFrom(results, debouncedSearch);
 			}
 
-			updatedResults.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+			updatedResults.sort(
+				(a, b) =>
+					new Date(b.timestamp).getTime() -
+					new Date(a.timestamp).getTime(),
+			);
 
 			setFilteredResults(updatedResults);
 		}, 300); // 300ms debounce delay for better ux
@@ -49,21 +51,25 @@ const SideDrawer: React.FC<SideDrawerProps> = ({ content }) => {
 		<SheetContent className="z-[1000]">
 			<SheetHeader className="mb-3">
 				<SheetTitle>Listaus tapahtumista</SheetTitle>
-				<Input placeholder="Tyyppi, paikka, aika" value={debouncedSearch} onChange={handleChange} />
+				<Input
+					placeholder="Tyyppi, paikka, aika"
+					value={debouncedSearch}
+					onChange={handleChange}
+				/>
 			</SheetHeader>
 			<div className="overflow-y-auto max-h-full">
-				{filteredResults.map(item => {
+				{filteredResults.map((item) => {
 					return (
 						<div key={item.id}>
-							<span className="font-bold">{item.location}</span> <span>{item.timestamp}</span>
+							<span className="font-bold">{item.location}</span>{' '}
+							<span>{item.timestamp}</span>
 							<p className="mb-4">{item.type}</p>
 						</div>
-					)
+					);
 				})}
 			</div>
 		</SheetContent>
-
-	)
-}
+	);
+};
 
 export default SideDrawer;
